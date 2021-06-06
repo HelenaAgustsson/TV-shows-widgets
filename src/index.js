75d162f618c24848484daa825be595e4a742904f
Helena Agustsson <you@example.com>
2021-06-06 12:29:52 +0100

    set up TV app with widgets

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Component } from 'react-simplified';
 import ReactDOM from 'react-dom';
 import { NavLink, HashRouter, Route } from 'react-router-dom';
-import { studentService } from './services';
+import { studentService, showService } from './services';
 import { Alert, Card, Row, Column, NavBar, Button, Form } from './widgets';
 import { createHashHistory } from 'history';
 
@@ -11,8 +11,8 @@ const history = createHashHistory(); // Use history.push(...) to programmaticall
 class Menu extends Component {
   render() {
     return (
-      <NavBar brand="StudAdm">
-        <NavBar.Link to="/students">Students</NavBar.Link>
+      <NavBar brand="TVAdm">
+        <NavBar.Link to="/shows">TV Shows</NavBar.Link>
       </NavBar>
     );
   }
@@ -20,50 +20,78 @@ class Menu extends Component {
 
 class Home extends Component {
   render() {
-    return <Card title="Welcome">Welcome to StudAdm</Card>;
+    return <Card title="Welcome">Welcome to TV Central</Card>;
   }
 }
 
-class StudentList extends Component {
-  students = [];
+class TVList extends Component {
+  shows = [];
 
   render() {
     return (
-      <Card title="Students">
-        {this.students.map((student) => (
-          <Row key={student.id}>
-            <Column>
-              <NavLink to={'/students/' + student.id}>{student.name}</NavLink>
-            </Column>
-          </Row>
-        ))}
-      </Card>
+      <div>
+        <Card title="Shows">
+          {this.shows.map((show) => (
+            <Row key={show.id}>
+              <Column>
+                <NavLink to={'/shows/' + show.id}>{show.title}</NavLink>
+              </Column>
+            </Row>
+          ))}
+        </Card>
+        <button onClick={this.add}>Add new show</button>
+      </div>
     );
   }
-
   mounted() {
-    studentService.getStudents((students) => {
-      this.students = students;
+    showService.getShows((shows) => {
+      this.shows = shows;
     });
+  }
+  add() {
+    history.push('shows/new');
   }
 }
 
-class StudentDetails extends Component {
-  student = null;
+class TVDetails extends Component {
+  show = null;
+  showRatings = [];
 
   render() {
-    if (!this.student) return null;
+    if (!this.show) return null;
 
     return (
       <div>
-        <Card title="Student details">
+        <Card title="TV show details">
           <Row>
-            <Column width={2}>Name:</Column>
-            <Column>{this.student.name}</Column>
+            <Column width={3}>Title:</Column>
+            <Column>{this.show.title}</Column>
           </Row>
           <Row>
-            <Column width={2}>Email:</Column>
-            <Column>{this.student.email}</Column>
+            <Column width={3}>Description:</Column>
+            <Column>{this.show.description}</Column>
+          </Row>
+          {/*
+          <Row>
+            <Column width={2}>Reviews:</Column>
+            <Column>
+              {this.showRatings.map((showRating) => (
+                <div key={showRating.id}>{showRating.rating}</div>
+              ))}
+            </Column>
+          </Row>
+            */}
+          <Row>
+            <Column width={4}>Average Rating:</Column>
+            <Column>
+              {Math.round(
+                this.showRatings.reduce(
+                  (average, showRating, _index, showRatings) =>
+                    average + showRating.rating / showRatings.length,
+                  0
+                )
+              )}
+            </Column>
           </Row>
         </Card>
         <Button.Light onClick={this.edit}>Edit</Button.Light>
@@ -72,13 +100,76 @@ class StudentDetails extends Component {
   }
 
   mounted() {
-    studentService.getStudent(this.props.match.params.id, (student) => {
-      this.student = student;
+    showService.getShow(this.props.match.params.id, (show) => {
+      this.show = show;
+    });
+    showService.getRatings(this.props.match.params.id, (ratings) => {
+      this.showRatings = ratings;
     });
   }
 
   edit() {
-    history.push('/students/' + this.student.id + '/edit');
+    history.push('/shows/' + this.show.id + '/edit');
+  }
+}
+
+class TVNew extends Component {
+  render() {
+    if (!this.show) return null;
+
+    return (
+      <div>
+        <Card title="New TV Show"></Card>
+      </div>
+    );
+  }
+  mounted() {}
+}
+
+class TVEdit extends Component {
+  show = null;
+  myRating = 0;
+
+  render() {
+    if (!this.show) return null;
+
+    return (
+      <div>
+        <Card title="Rate TV Show">
+          <Row>
+            <Column width={3}>Title:</Column>
+            <Column>{this.show.title}</Column>
+          </Row>
+          <Row>
+            <Column width={3}>Description:</Column>
+            <Column>{this.show.description}</Column>
+          </Row>
+          <Column width={5}>
+            <Form.Label>Your rating:</Form.Label>{' '}
+          </Column>
+          <Column>
+            <input
+              type="number"
+              min="0"
+              max="5"
+              onChange={(event) => (this.myRating = event.currentTarget.value)}
+            />
+          </Column>
+        </Card>
+        <button onClick={this.rateShow}>Add my rating</button>
+      </div>
+    );
+  }
+  mounted() {
+    showService.getShow(this.props.match.params.id, (show) => {
+      this.show = show;
+    });
+  }
+  rateShow() {
+    console.log(this.myRating);
+    showService.addRating(this.show.id, this.myRating, () => {
+      history.push('/shows');
+    });
   }
 }
 
@@ -140,8 +231,10 @@ ReactDOM.render(
       <div>
         <Menu />
         <Route exact path="/" component={Home} />
-        <Route exact path="/students" component={StudentList} />
-        <Route exact path="/students/:id" component={StudentDetails} />
+        <Route exact path="/shows" component={TVList} />
+        <Route exact path="/shows/:id" component={TVDetails} />
+        <Route exact path="/shows/:id/edit" component={TVEdit} />
+        <Route exact path="/shows/new" component={TVNew} />
         <Route exact path="/students/:id/edit" component={StudentEdit} />
       </div>
     </HashRouter>
--- src/services.js
+++ src/services.js
@@ -1,8 +1,41 @@
 import { pool } from './mysql-pool';
 
+class ShowService {
+  getShows(success) {
+    pool.query('SELECT * FROM Shows', (error, results) => {
+      if (error) return console.error(error);
+
+      success(results);
+    });
+  }
+  getShow(id, success) {
+    pool.query('SELECT * FROM Shows WHERE id=?', [id], (error, results) => {
+      if (error) return console.error(error);
+
+      success(results[0]);
+    });
+  }
+  getRatings(showId, success) {
+    pool.query('SELECT * FROM ShowRatings WHERE ShowId=?', [showId], (error, results) => {
+      if (error) return console.log(error);
+      success(results);
+    });
+  }
+  addRating(showId, showRating, success) {
+    pool.query(
+      'INSERT INTO ShowRatings (rating, showId) VALUES (?,?)',
+      [showRating, showId],
+      (error, results) => {
+        if (error) return console.log(error);
+        success();
+      }
+    );
+  }
+}
+
 class StudentService {
   getStudents(success) {
-    pool.query('SELECT * FROM Students', (error, results) => {
+    pool.query('SELECT * FROM Shows', (error, results) => {
       if (error) return console.error(error);
 
       success(results);
@@ -29,4 +62,6 @@ class StudentService {
     );
   }
 }
+
+export let showService = new ShowService();
 export let studentService = new StudentService();
